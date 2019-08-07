c7fecfb2bb78bcd665475a1dfc32251408f4b3cf
Suzuki Mariko <>
2019-08-07 10:54:12 +0800

    initial commit

---
+++ GoogleSignInProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoogleSignInProject
+//
+//  Created by Suzuki Mariko on 07/08/2019.
+//  Copyright © 2019 Mariko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
